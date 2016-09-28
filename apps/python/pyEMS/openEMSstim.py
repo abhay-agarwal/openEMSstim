70741ff447587da91c7eb6d53ef040a7c8accc07
abhay-agarwal <abhay.agarwal@berkeley.edu>
2016-09-29 00:17:51 +0530

    new openems file

--- apps/python/pyEMS/openEMSstim.py
+++ apps/python/pyEMS/openEMSstim.py
@@ -9,33 +9,40 @@
 import serial
 from time import sleep
 import EMSCommand
+import bluepy.btle as btle
 version = '0.1'
+
+mac = "00:1E:C0:32:59:AD"
+service_uuid = '454d532d-5365-7276-6963-652d424c4531'
+
 
 class openEMSstim():
 
-    def __init__(self, serial_port,baudrate=9600):
+    def __init__(self, serial_port, baudrate=9600):
             self.serial_port = serial_port
             self.baudrate = baudrate
             self.sleep_wait = 10
             self.ems_device = None
-            
-            #open the connection
+
+            # open the connection
             try:
-                self.ems_device = serial.Serial(self.serial_port, self.baudrate) 
-            except serial.serialutil.SerialException: 
-                print("Exception Raised: serial device: " + str(self.serial_port) + " was not found")
-                return None
+                p = btle.Peripheral(mac)
+                srvs = p.getServices()
+                srv = next((s for s in srvs if str(s.uuid) == service_uuid), None)
+                if srv is not None:
+                    # print srv
+                    # print str(srv.uuid)
+                    self.ems_device = srv.getCharacteristics()[0]
+                else:
+                    print("Uh oh! I couldn't find the EMS device!")
+            except Exception as e:
+                print("Looks like something went wrong")
 
-            # wait for n seconds
-            print("Waiting " + str(self.sleep_wait) + " seconds for the board to setup...\n")
-            sleep(self.sleep_wait)
-            print("Ready.\n")
-
-    def send(self,ems_stimulation_command):
-            if self.ems_device and ems_stimulation_command:      
+    def send(self, ems_stimulation_command):
+            if self.ems_device and ems_stimulation_command:
                 self.ems_device.write(str(ems_stimulation_command))
             else:
-                print("Problem creating EMS device on: " + str(self.serial_port) + " with command: " + ems_stimulation_command)
+                print("Problem with command: " + ems_stimulation_command)
 
     def shutdown(self):
         self.ems_device.close()
